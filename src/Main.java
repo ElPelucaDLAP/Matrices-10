e0f7a2f1c695eefdcef7bf38ce23cac206b22d36
ElPeluc4 <agusnarsaniga@proton.me>
2025-07-10 17:09:19 -0300

    3
    a

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,47 @@ public class Main {
         eliminarRepeticiones(matriz,MAXFILA,MAXCOLUMNA);
     }
     public static void eliminarRepeticiones (int[][] matriz, final int MAXFILA, final int MAXCOLUMNA){
+        int[] diasAlzaMesX ={0,0,0};
       for (int i = 0; i<MAXFILA; i++){
           int inicioSecuencia = 0;
           int finSecuencia = -1;
+          int finSecuenciaDiaAnterior=0;
+          int contadorDiasEnAlza=0;
+          int tamano;
+          int tato = 0;
           while (inicioSecuencia<MAXCOLUMNA){
               inicioSecuencia=buscarInicio(MAXCOLUMNA,matriz[i],finSecuencia+1);
               if (inicioSecuencia<MAXCOLUMNA){
                   finSecuencia=buscarFin(MAXCOLUMNA,matriz[i],inicioSecuencia);
               }
+              if (tato==0){
+                  if (matriz[i][inicioSecuencia]<matriz[i][finSecuencia]) {
+                   contadorDiasEnAlza++;
+                   finSecuenciaDiaAnterior=finSecuencia;
+                  }
+                  tato++;
+              }else if (finSecuenciaDiaAnterior<finSecuencia){
+                  finSecuenciaDiaAnterior=finSecuencia;
+                  contadorDiasEnAlza++;
+              }else {finSecuenciaDiaAnterior=finSecuencia;}
+              tamano=(finSecuencia-inicioSecuencia+1);
+              eliminarRepetidos(inicioSecuencia,finSecuencia,matriz[i],tamano);
           }
+          diasAlzaMesX[i]=contadorDiasEnAlza;
       }
+      for (int r = 0; r<=2;r++){
+          System.out.println("Mes: "+(r+1)+" cantidad de dias al alza: "+diasAlzaMesX[r]);
+      }
+    }
+    public static void eliminarRepetidos (int inicio,int fin, int[] array, int tamano) {
+        int numeroAnterior = 0;
+        while (inicio<=fin){
+            if (array[inicio]==array[inicio-1] || numeroAnterior==array[inicio] ){
+                numeroAnterior=array[inicio];
+                array[inicio]=0;
+            }
+            inicio++;
+        }
     }
     public static int buscarInicio (final int MAXCOLUMNA, int[]array, int fin) {
         int inicio = fin;
